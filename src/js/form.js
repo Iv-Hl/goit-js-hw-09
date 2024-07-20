5d50f6f3afe8268b2cc0ef36aecddaa93b3378f9
Ivan Hlieb <2020ivanko14@gmail.com>
2024-07-20 02:26:57 +0200

    finally done hw js 9

---
+++ src/js/form.js
@@ -0,0 +1,48 @@
+const formData = {
+  email: '',
+  message: ''
+};
+
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+});
+
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+
+form.addEventListener('input', (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  saveToLocalStorage();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
